1adf3a8e92204d36e1f5ca8df3d8aa49403379b7
reshma poudel <reshmapoudel815@gmail.com>
2025-02-19 22:26:16 +0545

    fix th e problem in layout sectio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,34 @@
-import "./globals.css";
 import Footer from "../components/Footer";
+import "./globals.css";
+// import Footer from "../components/Footer";
 
-// export default function RootLayout({
-//   // Layouts must accept a children prop.
-//   // This will be populated with nested layouts or pages
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <body>{children}</body>
-//       <Footer />
-//     </html>
-//   );
-// }
-
-// layout.tsx
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div>
-      {children}
-      <Footer />
-    </div>
+    <html lang="en">
+      <body>
+        {children}
+        <Footer />
+      </body>
+
+      {/* <footer>
+        <Footer />
+      </footer> */}
+    </html>
   );
 }
+
+// layout.tsx
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <div>
+//       {children}
+//       <Footer />
+//     </div>
+//   );
+// }
